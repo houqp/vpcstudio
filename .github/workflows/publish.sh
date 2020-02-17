e6d3e657a5bcae0454a6de08d72a6493928da20b
Qingping Hou <dave2008713@gmail.com>
2020-02-16 22:28:34 -0800

    fix github commit

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -4,6 +4,11 @@ set -e
 
 REPOSITORY=$GITHUB_REPOSITORY
 
+[ -z "${GITHUB_TOKEN}" ] && {
+    echo 'Missing environment variable GITHUB_TOKEN.';
+    exit 1;
+};
+
 make build
 
 echo "pushing to gh-pages branch"
@@ -13,7 +18,9 @@ git checkout --orphan gh-pages
 git rm -rf .
 cp dist/* ./
 git add *.js *.css *.html
-git commit -m 'github pages release'
 
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
+git commit -m 'github pages release'
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 git push -f "${remote_repo}" HEAD:gh-pages
