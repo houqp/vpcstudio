3210ceb187096874dd0468f6e41371e6e32f743c
Qingping Hou <dave2008713@gmail.com>
2020-02-22 22:05:40 -0800

    refactor terraform code gen

--- src/terraform_view.ts
+++ src/terraform_view.ts
@@ -1,5 +1,6 @@
 import {
     Cluster,
+    VPC,
 } from "./planner";
 
 interface VPCTemplateVars {
@@ -10,32 +11,56 @@ interface VPCTemplateVars {
     private_subnets: string[];
 }
 
-function renderVPCTemplate(opts: VPCTemplateVars): string {
-    const private_subnets = [];
-    for (const cidr of opts.private_subnets) {
-        private_subnets.push(`"${cidr}"`);
+function renderVPCSubnetRoutes(vpc: VPC): string {
+    const subnet_cidrs: { [index: string]: string[]} = {};
+    for (const zone of vpc.zones) {
+        for (const subnet of zone.subnets) {
+            if (!subnet_cidrs.hasOwnProperty(subnet.name)) {
+                subnet_cidrs[subnet.name] = [];
+            }
+            subnet_cidrs[subnet.name].push(`"${subnet.cidr}"`);
+        }
+    }
+
+    const parts = [`    "${vpc.name}" = {`
+    ];
+
+    for (const route of Object.keys(subnet_cidrs)) {
+        const cidrs = subnet_cidrs[route];
+        parts.push(`      "${route}" = [${cidrs.join(", ")}]`);
+    }
+
+    parts.push(`    }`);
+
+    return parts.join("\n");
+}
+
+function renderVPCTemplate(vpc: VPC): string {
+    const zones = [];
+    for (const zone of vpc.zones) {
+        zones.push(`"${zone.zone}"`);
     }
 
     const public_subnets = [];
-    for (const cidr of opts.public_subnets) {
-        public_subnets.push(`"${cidr}"`);
-    }
-
-    const zones = [];
-    for (const zone of opts.zones) {
-        zones.push(`"${zone}"`);
+    const private_subnets = [];
+    for (const route_name of Object.keys(vpc.subnet_routes)) {
+        if (route_name === "public" || route_name.startsWith("public_")) {
+            public_subnets.push(`local.subnet_routes["${vpc.name}"]["${route_name}"]`);
+        } else {
+            private_subnets.push(`local.subnet_routes["${vpc.name}"]["${route_name}"]`);
+        }
     }
 
     return `
-module "vpc-${opts.name}" {
+module "vpc-${vpc.name}" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "${opts.name}"
-  cidr = "${opts.cidr}"
+  name = "${vpc.name}"
+  cidr = "${vpc.cidr}"
 
   azs             = [${zones.join(", ")}]
-  private_subnets = [${private_subnets.join(", ")}]
-  public_subnets  = [${public_subnets.join(", ")}]
+  private_subnets = concat(${private_subnets.join(", ")})
+  public_subnets  = concat(${public_subnets.join(", ")})
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -51,31 +76,21 @@ function drawTerraform(cluster: Cluster): void {
         `    Terraform = "true"`,
         `    Source    = "vpcstudio"`,
         `  }`,
-        "}",
+        ``,
+        `  subnet_routes = {`,
     ];
 
+    // render local variables
     for (const vpc of cluster.vpcs) {
-        const vpc_opts = {
-            name: vpc.name,
-            cidr: vpc.cidr,
-            zones: [] as string[],
-            public_subnets: [] as string[],
-            private_subnets: [] as string[],
-        };
+        code_parts.push(renderVPCSubnetRoutes(vpc));
+    }
 
-        for (const zone of vpc.zones) {
-            vpc_opts.zones.push(zone.zone);
+    code_parts.push("  }");
+    code_parts.push("}");
 
-            for (const subnet of zone.subnets) {
-                if (subnet.name === "public" || subnet.name.startsWith("public_")) {
-                    vpc_opts.public_subnets.push(subnet.cidr);
-                } else {
-                    vpc_opts.private_subnets.push(subnet.cidr);
-                }
-            }
-        }
-
-        code_parts.push(renderVPCTemplate(vpc_opts));
+    // render module call
+    for (const vpc of cluster.vpcs) {
+        code_parts.push(renderVPCTemplate(vpc));
     }
 
     const code = code_parts.join("\n");
